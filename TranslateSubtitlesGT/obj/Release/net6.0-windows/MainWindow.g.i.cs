95b3c3b75671b3b7f29f98b7b1a633f4ee1a67c0
olegm <olegmedvmail@gmail.com>
2023-02-18 20:28:42 +0200

    remove un

--- TranslateSubtitlesGT/obj/Release/net6.0-windows/MainWindow.g.i.cs
+++ TranslateSubtitlesGT/obj/Release/net6.0-windows/MainWindow.g.i.cs
@@ -133,7 +133,7 @@ namespace TranslateSubtitlesGT {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/TranslateSubtitlesGT;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/TranslateSubtitlesGT;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
